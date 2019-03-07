2d3a95db5696a7b6218ab2b3cd75cf62ffff5045
Eliot <eliotcrespin@live.fr>
2019-03-07 11:32:10 +0100

    same batch for log and training

--- generator_training.py
+++ generator_training.py
@@ -27,7 +27,7 @@ def train_generator():
         tf.float32, [None, Height, Width, 1], name="covers")
     rand_maps = tf.placeholder(
         tf.float32, [None, Height, Width, 1], name="rand_maps")
-    generator = GeneratorModel(covers, rand_maps)
+    generator = GeneratorModel(covers, batch_size, rand_maps)
 
     saver = tf.train.Saver()
 
@@ -37,14 +37,14 @@ def train_generator():
 
         # Create a merged_summary and a separate image summary
         merged_summary = tf.summary.merge_all()
-        image_summaries = [tf.summary.image("cover", covers, max_outputs=2),
+        image_summaries = [tf.summary.image("cover", covers[:2], max_outputs=2),
                            tf.summary.image(
-                               "costs_map", generator.generator_prediction, max_outputs=2),
+                               "costs_map", generator.generator_prediction[:2], max_outputs=2),
                            tf.summary.image("modification_map",
-                                            tf.abs(generator.tesModel.tes_prediction), max_outputs=2),
-                           tf.summary.image("stego", generator.tesModel.generate_image, max_outputs=2)]
+                                            tf.abs(generator.tesModel.tes_prediction[:2]), max_outputs=2),
+                           tf.summary.image("stego", generator.tesModel.generate_image[:2], max_outputs=2)]
         image_summary = tf.summary.merge(image_summaries)
-        summaries_dir = "./.tensorboards-logs/gan/v1/"
+        summaries_dir = "./.tensorboards-logs/gan/v2/"
         writer = tf.summary.FileWriter(summaries_dir)
         writer.add_graph(sess.graph)
 
@@ -67,17 +67,17 @@ def train_generator():
             if (iteration % log_images_every == 0):
                 # Run the image summary at the start of each batch
                 s = sess.run(image_summary,
-                             {covers: first_images, rand_maps=rand_maps_batch})
+                             {covers: first_images, rand_maps: rand_maps_batch})
                 writer.add_summary(s, iteration)
 
             if (iteration % log_scalars_every == 0 and iteration != 0):
                 # Compute errors and loss, add to summary and log
                 (loss_disc, num_diff) = sess.run(generator.yedroudjModel.error,
-                                                 {covers: cover_batch, rand_maps=rand_maps_batch})
+                                                 {covers: cover_batch, rand_maps: rand_maps_batch})
                 loss_gen = sess.run(generator.loss,
-                                    {covers: cover_batch, rand_maps=rand_maps_batch})
+                                    {covers: cover_batch, rand_maps: rand_maps_batch})
                 s = sess.run(merged_summary,
-                             {covers: cover_batch, rand_maps=rand_maps_batch})
+                             {covers: cover_batch, rand_maps: rand_maps_batch})
                 writer.add_summary(s, iteration)
 
                 print('\n\n{:6.2f}% of current epoch'.format(
@@ -92,9 +92,9 @@ def train_generator():
 
             # OPTIMIZE
             sess.run(generator.yedroudjModel.optimize,
-                     {covers: cover_batch, rand_maps=rand_maps_batch})
+                     {covers: cover_batch, rand_maps: rand_maps_batch})
             sess.run(generator.optimize,
-                     {covers: cover_batch, rand_maps=rand_maps_batch})
+                     {covers: cover_batch, rand_maps: rand_maps_batch})
 
             iteration += 1
 
